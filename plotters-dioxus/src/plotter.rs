a05749305a5b263c5b4ad2911327dd7188464e85
czarop <151532746+czarop@users.noreply.github.com>
2024-12-04 18:21:22 +0000

    Update plotter.rs

--- plotters-dioxus/src/plotter.rs
+++ plotters-dioxus/src/plotter.rs
@@ -3,11 +3,11 @@
 use dioxus::prelude::*;
 
 use plotters_bitmap::BitMapBackend;
-
 use plotters::prelude::*;
 use plotters::coord::Shift;
 
-use base64::prelude::*;
+use base64::Engine as _;
+use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
 
 use image::ImageEncoder;
 use image::codecs::png::PngEncoder;
@@ -20,139 +20,81 @@ pub type DioxusDrawingArea<'a> = DrawingArea<BitMapBackend<'a>, Shift>;
 pub struct PlottersProps<'a, F: Fn(DioxusDrawingArea)> {
     pub size: (u32, u32),
     pub init: F,
-    pub on_click: Option<EventHandler<'a, Event<MouseData>>>,
-    pub on_dblclick: Option<EventHandler<'a, Event<MouseData>>>,
-    pub on_mousemove: Option<EventHandler<'a, Event<MouseData>>>,
-    pub on_mouseout: Option<EventHandler<'a, Event<MouseData>>>,
-    pub on_mouseup: Option<EventHandler<'a, Event<MouseData>>>,
-    pub on_mousedown: Option<EventHandler<'a, Event<MouseData>>>,
-    pub on_mouseover: Option<EventHandler<'a, Event<MouseData>>>,
-    pub on_wheel: Option<EventHandler<'a, Event<WheelData>>>,
+    #[props(optional)]
+    pub on_click: Option<EventHandler<'a, MouseData>>,
+    #[props(optional)]
+    pub on_dblclick: Option<EventHandler<'a, MouseData>>,
+    #[props(optional)]
+    pub on_mousemove: Option<EventHandler<'a, MouseData>>,
+    #[props(optional)]
+    pub on_mouseout: Option<EventHandler<'a, MouseData>>,
+    #[props(optional)]
+    pub on_mouseup: Option<EventHandler<'a, MouseData>>,
+    #[props(optional)]
+    pub on_mousedown: Option<EventHandler<'a, MouseData>>,
+    #[props(optional)]
+    pub on_mouseover: Option<EventHandler<'a, MouseData>>,
+    #[props(optional)]
+    pub on_wheel: Option<EventHandler<'a, WheelData>>,
     #[props(default = false)]
     pub draggable: bool,
-    pub on_drag: Option<EventHandler<'a, Event<DragData>>>,
-    pub on_dragend: Option<EventHandler<'a, Event<DragData>>>,
-    pub on_dragenter: Option<EventHandler<'a, Event<DragData>>>,
-    pub on_dragleave: Option<EventHandler<'a, Event<DragData>>>,
-    pub on_dragover: Option<EventHandler<'a, Event<DragData>>>,
-    pub on_dragstart: Option<EventHandler<'a, Event<DragData>>>,
-    pub on_drop: Option<EventHandler<'a, Event<DragData>>>,
-    pub on_scroll: Option<EventHandler<'a, Event<ScrollData>>>,
-}
-
-impl<'a, F: Fn(DioxusDrawingArea)> PartialEq for PlottersProps<'a, F> {
-    fn eq(&self, other: &PlottersProps<F>) -> bool {
-        self.size == other.size
-    }
+    #[props(optional)]
+    pub on_drag: Option<EventHandler<'a, DragData>>,
+    #[props(optional)]
+    pub on_dragend: Option<EventHandler<'a, DragData>>,
+    #[props(optional)]
+    pub on_dragenter: Option<EventHandler<'a, DragData>>,
+    #[props(optional)]
+    pub on_dragleave: Option<EventHandler<'a, DragData>>,
+    #[props(optional)]
+    pub on_dragover: Option<EventHandler<'a, DragData>>,
+    #[props(optional)]
+    pub on_dragstart: Option<EventHandler<'a, DragData>>,
+    #[props(optional)]
+    pub on_drop: Option<EventHandler<'a, DragData>>,
+    #[props(optional)]
+    pub on_scroll: Option<EventHandler<'a, ScrollData>>,
 }
 
 pub fn Plotters<'a, F: Fn(DioxusDrawingArea)>(cx: Scope<'a, PlottersProps<'a, F>>) -> Element<'a> {
     let buffer_size = ((cx.props.size.1 * cx.props.size.0) as usize) * 3usize;
     let mut buffer = vec![0u8; buffer_size];
-    let drawing_area = BitMapBackend::with_buffer(
-        buffer.as_mut_slice(),
-        cx.props.size
-    ).into_drawing_area();
+    let drawing_area = BitMapBackend::with_buffer(buffer.as_mut_slice(), cx.props.size)
+        .into_drawing_area();
     (cx.props.init)(drawing_area);
 
-    let mut data = vec![0; 0];
+    let mut data = Vec::new();
     let cursor = Cursor::new(&mut data);
     let encoder = PngEncoder::new(cursor);
     let color = image::ColorType::Rgb8;
 
     encoder
         .write_image(buffer.as_slice(), cx.props.size.0, cx.props.size.1, color)
-        .expect("The Png encoder is expected to write the image");
+        .expect("Failed to write the image");
 
     let buffer_base64 = BASE64_STANDARD.encode(data);
 
-    render!(img {
-        onclick: |e| {
-            if let Some(callback) = cx.props.on_click.as_ref() {
-                callback.call(e)
-            };
-        },
-        ondblclick: |e| {
-            if let Some(callback) = cx.props.on_dblclick.as_ref() {
-                callback.call(e)
-            };
-        },
-        onmousemove: |e| {
-            if let Some(callback) = cx.props.on_mousemove.as_ref() {
-                callback.call(e)
-            };
-        },
-        onmousedown: |e| {
-            if let Some(callback) = cx.props.on_mousedown.as_ref() {
-                callback.call(e)
-            };
-        },
-        onmouseup: |e| {
-            if let Some(callback) = cx.props.on_mouseup.as_ref() {
-                callback.call(e)
-            };
-        },
-        onmouseout: |e| {
-            if let Some(callback) = cx.props.on_mouseout.as_ref() {
-                callback.call(e)
-            };
-        },
-        onmouseover: |e| {
-            if let Some(callback) = cx.props.on_mouseout.as_ref() {
-                callback.call(e)
-            };
-        },
-        onwheel: |e| {
-            if let Some(callback) = cx.props.on_wheel.as_ref() {
-                callback.call(e)
-            };
-        },
-        onwheel: |e| {
-            if let Some(callback) = cx.props.on_wheel.as_ref() {
-                callback.call(e)
-            };
-        },
-        draggable: cx.props.draggable,
-        ondrag: |e| {
-            if let Some(callback) = cx.props.on_drag.as_ref() {
-                callback.call(e)
-            };
-        },
-        ondragend: |e| {
-            if let Some(callback) = cx.props.on_dragend.as_ref() {
-                callback.call(e)
-            };
-        },
-        ondragenter: |e| {
-            if let Some(callback) = cx.props.on_dragenter.as_ref() {
-                callback.call(e)
-            };
-        },
-        ondragleave: |e| {
-            if let Some(callback) = cx.props.on_dragleave.as_ref() {
-                callback.call(e)
-            };
-        },
-        ondragover: |e| {
-            if let Some(callback) = cx.props.on_dragover.as_ref() {
-                callback.call(e)
-            };
-        },
-        ondragstart: |e| {
-            if let Some(callback) = cx.props.on_dragstart.as_ref() {
-                callback.call(e)
-            };
-        },
-        ondrop: |e| {
-            if let Some(callback) = cx.props.on_drop.as_ref() {
-                callback.call(e)
-            };
-        },
-        onscroll: |e| {
-            if let Some(callback) = cx.props.on_scroll.as_ref() {
-                callback.call(e)
-            };
-        },
-        src: "data:image/png;base64,{buffer_base64}",
+    render!(rsx! {
+        img {
+            src: "data:image/png;base64,{buffer_base64}",
+            draggable: "{cx.props.draggable}",
+            onclick: move |evt| cx.props.on_click.as_ref().map(|cb| cb.call(evt)),
+            ondblclick: move |evt| cx.props.on_dblclick.as_ref().map(|cb| cb.call(evt)),
+            onmousemove: move |evt| cx.props.on_mousemove.as_ref().map(|cb| cb.call(evt)),
+            onmouseout: move |evt| cx.props.on_mouseout.as_ref().map(|cb| cb.call(evt)),
+            onmouseover: move |evt| cx.props.on_mouseover.as_ref().map(|cb| cb.call(evt)),
+            onmousedown: move |evt| cx.props.on_mousedown.as_ref().map(|cb| cb.call(evt)),
+            onmouseup: move |evt| cx.props.on_mouseup.as_ref().map(|cb| cb.call(evt)),
+            onwheel: move |evt| cx.props.on_wheel.as_ref().map(|cb| cb.call(evt)),
+            ondrag: move |evt| cx.props.on_drag.as_ref().map(|cb| cb.call(evt)),
+            ondragend: move |evt| cx.props.on_dragend.as_ref().map(|cb| cb.call(evt)),
+            ondragenter: move |evt| cx.props.on_dragenter.as_ref().map(|cb| cb.call(evt)),
+            ondragleave: move |evt| cx.props.on_dragleave.as_ref().map(|cb| cb.call(evt)),
+            ondragover: move |evt| cx.props.on_dragover.as_ref().map(|cb| cb.call(evt)),
+            ondragstart: move |evt| cx.props.on_dragstart.as_ref().map(|cb| cb.call(evt)),
+            ondrop: move |evt| cx.props.on_drop.as_ref().map(|cb| cb.call(evt)),
+            onscroll: move |evt| cx.props.on_scroll.as_ref().map(|cb| cb.call(evt)),
+        }
     })
 }
+
