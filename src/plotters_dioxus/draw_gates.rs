fe379fc71bde658feb7191829ebf41d384423cf1
czarop <151532746+czarop@users.noreply.github.com>
2026-02-04 13:15:57 +0000

    1

--- src/plotters_dioxus/draw_gates.rs
+++ src/plotters_dioxus/draw_gates.rs
@@ -35,7 +35,115 @@ pub fn GateLayer(
                     height: "100%",
                     view_box: "0 0 {&mapper.view_width} {&mapper.view_height}",
                     style: "position: absolute; top: 0; left: 0; pointer-events: none; z-index: 2;",
-                    onmousemove: 
+                    onmousemove: move |evt| {},
+                    for gate in &*gates.read() {
+                        match &gate.geometry {
+                            GateGeometry::Polygon { nodes: _, closed: _ } => {
+                                let is_selected = match selected_gate_id() {
+                                    Some(id) => if gate.id == id { true } else { false }
+                                    None => false,
+                                };
+                                let stroke = if is_selected { "red" } else { "cyan" };
+                                let points: Vec<(f32, f32)> = gate
+                                    .get_points()
+                                    .iter()
+                                    .map(|v| plot_map.as_ref().unwrap().map_to_svg(v.0, v.1))
+                                    .collect();
+                                let points_attr = points
+                                    .iter()
+                                    .map(|(px, py)| { format!("{px},{py}") })
+                                    .collect::<Vec<_>>()
+                                    .join(" ");
+
+                                rsx! {
+                                    polygon {
+                                        points: "{points_attr}",
+                                        fill: "rgba(0, 255, 255, 0.2)",
+                                        stroke,
+                                        stroke_width: "2",
+                                        pointer_events: "none",
+                                    }
+                                    if is_selected {
+                                        for (idx , point) in points.iter().enumerate() {
+                                            circle {
+                                                key: "{gate.id}-{idx}",
+                                                cx: "{point.0}",
+                                                cy: "{point.1}",
+                                                r: "4",
+                                                fill: "red",
+                                                cursor: "move",
+                                                onmousedown: move |evt| {
+                                                    let local_coords = &evt.data.coordinates().element();
+                                                    let px = local_coords.x as f32;
+                                                    let py = local_coords.y as f32;
+                                                    let data_coords = plot_map.as_ref().unwrap().map_to_svg(px, py);
+                                                    drag_data.set(Some((idx, data_coords)));
+                                                },
+
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            _ => rsx! {},
+                        }
+                    }
+
+                    match draft_gate() {
+                        Some(draft) => {
+                            let mut points_attr = draft
+                                .get_points()
+                                .iter()
+                                .map(|v| {
+                                    let (px, py) = mapper.map_to_svg(v.0, v.1);
+                                    format!("{px},{py}")
+                                })
+                                .collect::<Vec<_>>();
+                            if let Some(first) = points_attr.first() && points_attr.len() > 2 {
+                                points_attr.push(first.clone());
+                            }
+                            let draft_string = points_attr.join(" ");
+
+                            match points_attr.len() {
+                                0 => rsx! {},
+                                1 => {
+                                    let points: Vec<&str> = draft_string.split(",").collect();
+                                    rsx! {
+                                        circle {
+                                            cx: "{points[0]}",
+                                            cy: "{points[1]}",
+                                            r: "3",
+                                            fill: "red",
+
+                                        }
+                                    }
+                                }
+                                2 => rsx! {
+                                    polyline {
+                                        points: "{draft_string}",
+                                        stroke: "red",
+                                        stroke_width: "2",
+                                        fill: "none",
+
+                                    }
+                                },
+                                _ => rsx! {
+                                    polygon {
+                                        points: "{draft_string}",
+                                        fill: "rgba(0, 255, 255, 0.2)",
+                                        stroke: "red",
+                                        stroke_width: "2",
+                                        pointer_events: "none",
+                                    }
+                                },
+
+                            }
+                        }
+                        None => rsx! {},
+                    }
+
+                }
             },
             None => rsx! {},
         }
